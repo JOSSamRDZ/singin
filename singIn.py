87859ead4cff91890afd0f08d198c56087317010
Sam <loboabel.cronox@gmail.com>
2024-02-07 16:33:55 -0600

    Terminado el modulo 1 sirve la validacion del usuario

--- singIn.py
+++ singIn.py
@@ -1,4 +1,17 @@
-userName = input("User name:\n")
+
 serverUsersName= "fulanito"
 trialsOfUserName=0
-
+#While para el numero de intentos de ingresar al usuario correcto
+while trialsOfUserName <=4:
+    userName = input("User name:\n")
+    if userName == serverUsersName:
+        print("Aqui comienza el modulo 2")
+        break
+    else:
+        if trialsOfUserName < 4:
+            print("Nombre de usuario incorrecto, vuelva a intenrarlo de nuevo")
+            print(f"intentos restantes: {4-trialsOfUserName}")
+            trialsOfUserName+=1
+        else:
+            print("Numero de intentos superados, por favor espere 15 minutos")
+            break
